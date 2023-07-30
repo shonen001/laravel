48f0524bc57a73f7733d1f49ba8bbb91916f0323
shonen001 <monke.d.luffy27@gmail.com>
2023-07-30 14:56:23 +0100

    hmm

--- app/Http/Controllers/ContactServer.php
+++ app/Http/Controllers/ContactServer.php
@@ -1,76 +1,80 @@
 <?php
 
 namespace App\Http\Controllers;
-use  App\Models\contact;
-use  App\Models\group;
 
+use App\Models\contact;
+use App\Models\group;
 use Illuminate\Http\Request;
+
 
 class ContactServer extends Controller
 {
-   public function index() {
+    public function index()
+    {
 
-    $contacts = contact::orderBy('firstName')->where(function($query) {
-        if($IdGroup = request('groupID')) {
-             $query->where('idGroup',$IdGroup);
-        }})->paginate(5);
+        // \DB::enableQueryLog();
 
-         $groupsdropbox = group::orderBy('Group')->pluck('Group','id')->prepend('All Groups','');
-         return view('content.contacr', compact('contacts','groupsdropbox'));
-   }
-   public function PgCreateContact() {
-    $groupsdropbox = group::orderBy('Group')->pluck('Group','id')->prepend('Select a Group','');
-    return view('content.forms.formCreate' , compact('groupsdropbox'));
-   }
-   public function PgCreateContactInt( Request  $rqust) {
+        $contacts = contact::OrderrBy()->Filters()->paginate(10);
+        // dd(DB::getQueryLog());
 
-    $rqust->validate([
-            'firstName'=> 'required',
+        $groupsdropbox = group::DropBox('select a group');
+
+        return view('content.contacr', compact('contacts', 'groupsdropbox'));
+    }
+
+    public function addNew()
+    {
+        $groupsdropbox = group::orderBy('Group')->pluck('Group', 'id')->prepend('Select a Group', '');
+        return view('content.forms.formCreate', compact('groupsdropbox'));
+    }
+
+    public function store(Request $rqust)
+    {
+        $rqust->validate([
+            'firstName' => 'required',
             'lastName' => 'required',
-            'email'    => 'required|email',
-            'phone'    => 'required',
-            'address'  => 'required',
-            'idGroup'  => 'required|exists:grops,id'
+            'email' => 'required|email',
+            'phone' => 'required',
+            'address' => 'required',
+            'idGroup' => 'required|exists:grops,id'
         ]);
-       //dd($rqust->all());
+        //dd($rqust->all());
         contact::create($rqust->all());
 
+        return redirect()->route('Contact.home')->with('message', 'Contact has been add successfully');
+    }
 
-    return redirect()->route('Contact.home')->with('message','Contact has been add successfully');
-   }
-   public function showContact($id) {
-    $contact =contact::find($id);
-     return view('content/showContact',compact('contact'));
-   }
-   public function ShowEditeContact($id) {
-    $groupsdropbox = group::orderBy('Group')->pluck('Group','id')->prepend('','');
-    $contact =contact::find($id);
-     return view('content.forms._FrmEdite',compact('contact','groupsdropbox'));
-   }
-   public function SbmtEditeContact($id ,Request  $rqust) {
+    public function show(Contact $contact)
+    {
+        return view('content.showContact', compact('contact'));
+    }
 
-    $rqust->validate([
-        'firstName'=> 'required',
-        'lastName' => 'required',
-        'email'     => 'required|email',
-        'phone'     => 'required',
-        'address'   => 'required',
-        'idGroup'  => 'required|exists:grops,id'
-    ]);
+    public function edite($id)
+    {
+        $groupsdropbox = group::DropBox('');;
+        $contact = contact::find($id);
+        return view('content.forms._FrmEdite', compact('contact', 'groupsdropbox'));
+    }
+
+    public function update($id, Request $rqust)
+    {
+        $rqust->validate([
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required',
+            'address' => 'required',
+            'idGroup' => 'required|exists:grops,id '
+        ]);
         $contact = contact::find($id);
         $contact->update($rqust->all());
-        return redirect()->route('Contact.home')->with('message','Contact has been update successfully');
-   }
-   public function deletContact($id) {
+        return redirect()->route('Contact.home')->with('message', 'Contact has been update successfully');
+    }
 
-    $contact = contact::findOrFail($id);
-    $contact->delete();
-
-    return redirect()->route('Contact.home')->with('message','Contact has been deleate successfully');
-
-   }
-
+    public function destroy($id)
+    {
+        $contact = contact::findOrFail($id);
+        $contact->delete();
+        return redirect()->route('Contact.home')->with('message', 'Contact has been deleate successfully');
+    }
 }
-
-
-
